6de5d03a9ec64833a851567cc2575abac63e749e
anant <anantshukla@1234>
2020-04-20 02:11:38 +0530

    Updates

---
+++ precache-manifest.47e15c259bedd61b25498199e6eb2f37.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "163d0d192b152a562432",
+    "url": "/academic-credential-verification/static/css/main.6481217b.chunk.css"
+  },
+  {
+    "revision": "163d0d192b152a562432",
+    "url": "/academic-credential-verification/static/js/main.163d0d19.chunk.js"
+  },
+  {
+    "revision": "be725b4fdbf083d68f45",
+    "url": "/academic-credential-verification/static/css/1.afc6dc62.chunk.css"
+  },
+  {
+    "revision": "be725b4fdbf083d68f45",
+    "url": "/academic-credential-verification/static/js/1.be725b4f.chunk.js"
+  },
+  {
+    "revision": "697055fc2286b747984c",
+    "url": "/academic-credential-verification/static/js/runtime~main.697055fc.js"
+  },
+  {
+    "revision": "9a76e74e107e6163dc82116b8236c132",
+    "url": "/academic-credential-verification/static/media/login.9a76e74e.svg"
+  },
+  {
+    "revision": "77fdf036caf2b2891e6978341a2fc67b",
+    "url": "/academic-credential-verification/static/media/homepagewallpaper.77fdf036.png"
+  },
+  {
+    "revision": "2a194072ac0c29e5dfb0a3d4a84a37df",
+    "url": "/academic-credential-verification/index.html"
+  }
+];
